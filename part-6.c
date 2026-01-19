fb55325fd68154cd97fc0b68e0c77172805ca3d6
Charlie-264 <dkthanh2k6@gmail.com>
2026-01-19 02:11:44 +0000

    upload study files

---
+++ part-6.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    int a[] = {12, 2, 8, 5, 1, 6, 4, 15};
+    int n = sizeof(a) / sizeof(a[0]);
+
+    int temp, i, j;
+    for (i = 0; i < n-1; i++) {
+        for (j = i + 1; j < n; j++) {
+            if (a[j] < a[i]) {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+    printf("Dãy số tăng dần: \n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+}
